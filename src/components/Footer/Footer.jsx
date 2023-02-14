b3b4627f9d89021a29186de164c85f3ecf23b277
Miguel Camilo <miguelcamilo48@gmail.com>
2023-02-14 14:55:19 -0500

    added meta tags

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
 	return (
 		<footer className="mt-20">
 			<p className="text-blue-300 hover:-translate-x-0.5 hover:-translate-y-0.5 duration-300">
-				<a href="https://weather-wise-app.web.app" target="_blank">Source Code: Github</a>
+				<a href="https://github.com/MiguelCamilo/Weather-Wise-Web-App" target="_blank">Source Code: Github</a>
 			</p>
 		</footer>
 	);
